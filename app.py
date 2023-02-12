00a330c0b6a3698674f067ec63774cc0af8796ee
Keshav <kkarki1998@gmail.com>
2023-02-12 18:02:59 +0530

    added separate functionl buttons

--- app.py
+++ app.py
@@ -12,19 +12,96 @@ list_of_states =list(df['State'].unique())
 list_of_states.insert(0,'Overall India')
 list_of_states.insert(0,'select_one')
 
+#making select_one for both primary and secondary
+
+list_selection = sorted(list(df.columns[5:]))
+list_selection.insert(0,'Select_one')
+
 
 st.sidebar.title("Indias's census survey data")
 
 selected_state =st.sidebar.selectbox('select a state',list_of_states)
 
-primary_list = st.sidebar.selectbox("select primary parameter",  sorted(list(df.columns[5:]))) 
-secondary_list = st.sidebar.selectbox("select secondary parameter",sorted(list(df.columns[5:])))
+primary_list = st.sidebar.selectbox("select primary parameter",  list_selection) 
 
-plot = st.sidebar.button('Plot Graph')
+plot1 = st.sidebar.button('Plot graph for primary parameter')
+secondary_list = st.sidebar.selectbox("select secondary parameter",list_selection)
+
+# plot = st.sidebar.button('Plot graph')
+plot2 = st.sidebar.button('Plot graph for secondary parameter')
+
+st.sidebar.text("  ")
+st.sidebar.text("  ")
+st.sidebar.text("  ")
+
+plot3 = st.sidebar.button('Plot graph for both parameter')
 
 
 #Note:
-if plot:
+if plot1:
+    if selected_state == 'Overall India':
+        st.title("India's census - 2011 ")
+        st.text('text represent primary parameter')
+        st.text('color represent secondary parameter')
+
+        fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude',
+        size = primary_list ,
+        zoom=3.5,size_max=20,
+        mapbox_style='carto-positron', 
+        width=2000, height=700,
+        hover_name='District'
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+        
+    else:
+        st.title("India's census - 2011 ")
+        st.text('text represent primary parameter')
+        st.text('color represent secondary parameter')
+        state_df = df[df['State'] == selected_state]
+        fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude',
+        size = primary_list,
+        zoom=3.5,size_max=20,
+        mapbox_style='carto-positron', 
+        width=2000, height=800,
+        hover_name='District'
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+if plot2:
+    if selected_state == 'Overall India':
+        st.title("India's census - 2011 ")
+        st.text('text represent primary parameter')
+        st.text('color represent secondary parameter')
+
+        fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude',
+        size=secondary_list,
+        zoom=3.5,size_max=20,
+        mapbox_style='carto-positron', 
+        width=2000, height=700,
+        hover_name='District'
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+        
+    else:
+        st.title("India's census - 2011 ")
+        st.text('text represent primary parameter')
+        st.text('color represent secondary parameter')
+        state_df = df[df['State'] == selected_state]
+        fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude',
+        size=secondary_list,
+        zoom=3.5,size_max=20,
+        mapbox_style='carto-positron', 
+        width=2000, height=800,
+        hover_name='District'
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+
+if plot3:
     if selected_state == 'Overall India':
         st.title("India's census - 2011 ")
         st.text('text represent primary parameter')
@@ -54,3 +131,38 @@ if plot:
         )
 
         st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+# uncomment for both parameters together
+
+# if plot:
+#     if selected_state == 'Overall India':
+#         st.title("India's census - 2011 ")
+#         st.text('text represent primary parameter')
+#         st.text('color represent secondary parameter')
+
+#         fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude',
+#         size = primary_list ,color= secondary_list,
+#         zoom=3.5,size_max=20,
+#         mapbox_style='carto-positron', 
+#         width=2000, height=700,
+#         hover_name='District'
+#         )
+#         st.plotly_chart(fig, use_container_width=True)
+        
+#     else:
+#         st.title("India's census - 2011 ")
+#         st.text('text represent primary parameter')
+#         st.text('color represent secondary parameter')
+        
+#         state_df = df[df['State'] == selected_state]
+#         fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude',
+#         size = primary_list,color=secondary_list,
+#         zoom=3.5,size_max=20,
+#         mapbox_style='carto-positron', 
+#         width=2000, height=800,
+#         hover_name='District'
+#         )
+#         st.plotly_chart(fig, use_container_width=True)
